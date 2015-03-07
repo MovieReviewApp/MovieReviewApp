de0dd3ca980cb68a473da95bc69f9120ab01885d
Annie <anne.bourgeois1@gmail.com>
2015-03-06 21:46:40 -0500

    Set up the basic index.html file and App.init functions

--- app/js/app.js
+++ app/js/app.js
@@ -6,20 +6,50 @@ var trace = function(){
   }
 };
 
-var App = App || {};
-
-App.square = function(x){
-  return x * x
+var App = App || {
+    url: 'http://localhost:3000'
 };
 
-App.cube = function(x){
-  return x * x * x
+
+App.getMovies = function(){
+  $.ajax({
+    url: App.url + '/movies',
+    type: 'GET'
+  }).done(function(data){
+    App.indexMovies(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
 };
 
-App.greet = function(string){
-  return string = typeof string !== 'undefined' ? 'Hello ' + string : "Hello World";
+App.indexMovies = function(movies){
+  movies.forEach(App.renderMovie);
 };
+
+App.renderMovie = function(currentVal,index,array){
+  trace(currentVal, index, array);
+  for (var i = 0; i < 4; i++) {
+    $('.movie-list').append('<div class="row">');
+        $('.row').append('<div class="col-md-4">' + '<h1 class="movie-title">' + currentVal.title + '</h1>' + '<p class="movie-desc">' + currentVal.description + '</p>' + '<p>Number of reviews: ' + currentVal.reviews.length + '</p>' + '</div>');
+    $('.movie-list').append('</div>');
+  };
+
+};
+
 
 $(document).ready(function(){
-  trace('hello world');
+  $('#movie-search').on('change', function(event){
+    $('#sub-search').append('<select name="sub-category"><option value="">Select sub-category</option></select>');
+  });
+
+  App.getMovies();
+
+  // $('#movie-list').on('')
+  // $('#movie-search').on('change', function(event){
+  //   $('#sub-search').append('<select name="sub-category">');
+  //   $('#sub-search').append('<option value="">Select sub-category</option>');
+  //   $('#sub-search').append('</select>');
+  // });
 });
+
+
