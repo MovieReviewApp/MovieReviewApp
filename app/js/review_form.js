afc8006893560cd4eecd1b50036d27d27809b3ee
Annie <anne.bourgeois1@gmail.com>
2015-03-08 15:48:30 -0400

    Added a review form to add reviews to movies

---
+++ app/js/review_form.js
@@ -0,0 +1,68 @@
+/*global $:false*/
+'use strict';
+
+var trace = function(){
+  for(var i = 0; i < arguments.length; i++){
+    console.log(arguments[i]);
+  }
+};
+
+var App = App || {
+    url: 'http://localhost:3000'
+};
+
+// App.getReviews = function(movieId){
+//   $.ajax({
+//     url: App.url + '/movies/' + movieId.id + '/reviews',
+//     type: 'GET'
+//   }).done(function(data){
+//     // App.indexPosts(data);
+//     console.log(data);
+//   }).fail(function(jqXHR, textStatus, errorThrown){
+//     trace(jqXHR, textStatus, errorThrown);
+//   });
+// };
+
+App.updateReviews = function(data,movieId){
+    $.ajax({
+    url: 'http://localhost:3000/movies/' + movieId + '/reviews/' + data.id,
+    type: 'PATCH',
+    }).done(function(data){
+      trace(data);
+    }).fail();
+};
+
+App.submitReview = function(event,movieId){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: App.url + '/reviews',
+    type: 'POST',
+    data: {review:
+      {
+        star_rating: $('#star-rating').val(),
+        comment: $('#review-comment').val(),
+        username: $('#username').val()
+      }
+    },
+  }).done(function(data){
+    App.updateReviews(data,movieId);
+    trace(data,movieId);
+  }).fail(function(jqXKR, textStatus, errorThrown){
+    trace(jqXKR, textStatus, errorThrown);
+  })
+};
+
+
+$(document).ready(function(){
+  var locate = window.location.search;
+  var point = locate.lastIndexOf("=");
+  var movieId = parseInt(locate.substring(point+1,locate.length));
+
+  $('form.new-review-form').on('submit',function(event){
+    App.submitReview(event,movieId);
+  });
+});
+
+
+
+
